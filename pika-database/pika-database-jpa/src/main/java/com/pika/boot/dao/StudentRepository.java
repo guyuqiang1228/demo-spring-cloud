a1c3ea45e718553c0e3e672444c032b677e3c21a
guyuqiang <World74898me>
2021-03-15 16:58:55 +0800

    JPA测试增删改查方法

--- pika-database/pika-database-jpa/src/main/java/com/pika/boot/dao/StudentRepository.java
+++ pika-database/pika-database-jpa/src/main/java/com/pika/boot/dao/StudentRepository.java
@@ -2,9 +2,18 @@ package com.pika.boot.dao;
 
 import com.pika.boot.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
-public interface StudentRepository extends JpaRepository<Student,Integer> {
+/**
+ * @author guyuqiang
+ * @create 2021-03-15
+ */
+public interface StudentRepository extends JpaRepository<Student, Integer> {
 
-
+    @Query("update Student set name = ?1 where id = ?2")
+    //需要执行一个更新操作
+    @Modifying
+    void updateNameById(String name, Integer id);
 
 }
--- pika-database/pika-database-jpa/src/test/java/com/pika/boot/JpaApplicationTest.java
+++ pika-database/pika-database-jpa/src/test/java/com/pika/boot/JpaApplicationTest.java
@@ -4,9 +4,12 @@ import com.pika.boot.dao.StudentRepository;
 import com.pika.boot.entity.Student;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
 import javax.annotation.Resource;
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 public class JpaApplicationTest {
@@ -14,18 +17,40 @@ public class JpaApplicationTest {
     @Resource
     private StudentRepository studentRepository;
 
+    // 查询所有
     @Test
     public void testFindAll() {
         List<Student> all = studentRepository.findAll();
         System.out.println(all);
+
+        Optional<Student> studentOptional = studentRepository.findById(1);
+        Student student = studentOptional.get();
+        System.out.println(student);
+
     }
 
+    // 添加
     @Test
     public void testSave() {
         Student student = new Student();
         student.setName("李四");
         student.setAge(22);
         studentRepository.save(student);
+        System.out.println(123);
+    }
+
+    // 修改
+    @Test
+    @Transactional //@Transactional与@Test 一起使用时 事务是自动回滚的。
+    @Rollback(false) //取消自动回滚
+    public void testUpdate() {
+        studentRepository.updateNameById("555", 1);
+    }
+
+    // 删除
+    @Test
+    public void testDelete() {
+        studentRepository.deleteById(2);
         System.out.println(123);
     }
 
